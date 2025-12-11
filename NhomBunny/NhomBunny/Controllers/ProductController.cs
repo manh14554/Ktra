7aa68d8de4b99ab51048a0941602cb7627d1af60
thuowg203 <buithithuhuong06@gmail.com>
2025-12-11 08:52:08 +0700

    add controller/create

---
+++ NhomBunny/NhomBunny/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NhomBunny.Models;
+
+namespace NhomBunny.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public ProductController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(product);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
