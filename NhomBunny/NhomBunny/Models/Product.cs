e487b4f0b1656d0b7996241d51ff8c5ade35ba76
thuowg203 <buithithuhuong06@gmail.com>
2025-12-11 08:13:29 +0700

    add lai

---
+++ NhomBunny/NhomBunny/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace NhomBunny.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+    }
+}
